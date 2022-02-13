5fbb4cdc3800f6ab6aa957d1b1e40834644c0f83
DizelBadCoder <work.dizyaa@gmail.com>
2022-02-13 21:37:50 +0400

    Delete shared module

--- mobile/src/main/java/dev/dizel/discordintegration/MainActivity.kt
+++ mobile/src/main/java/dev/dizel/discordintegration/MainActivity.kt
@@ -6,15 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
 
@@ -37,13 +33,7 @@ private fun PreviewScreen() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Row {
-            Text(
-                fontSize = 30.sp,
-                textAlign = TextAlign.Center,
-                text = "120 BPM"
-            )
-        }
+
     }
 }
 
--- wear/src/main/java/dev/dizel/discordintegration/MainActivity.kt
+++ wear/src/main/java/dev/dizel/discordintegration/MainActivity.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.wear.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Device
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity: ComponentActivity() {
 
@@ -14,7 +21,22 @@ class MainActivity: ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text(text = "Text")
+            PreviewScreen()
         }
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    device = Device
+)
+@Composable
+private fun PreviewScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
     }
 }
