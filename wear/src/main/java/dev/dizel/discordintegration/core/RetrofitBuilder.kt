fdd6e2b88c461bbcc58ba52cecb73499327561dc
DizelBadCoder <diskroma69@gmail.com>
2022-04-10 12:03:07 +0400

    Feat: Move base url in config

--- wear/src/main/java/dev/dizel/discordintegration/core/RetrofitBuilder.kt
+++ wear/src/main/java/dev/dizel/discordintegration/core/RetrofitBuilder.kt
@@ -1,15 +1,13 @@
 package dev.dizel.discordintegration.core
 
+import dev.dizel.discordintegration.BuildConfig
 import dev.dizel.discordintegration.data.api.Requests
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
-
-    private const val BASE_URL = "http://192.168.1.67:8000/"
-
     private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
+        .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
